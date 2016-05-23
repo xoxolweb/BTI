a5e56ca1b43a4c880946dd8705625561e91f9d7e
xoxolweb <xoxol.web@gmail.com>
2016-05-23 15:48:26 +0300

    section 1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
   $(document).ready(function() {
-  //MENU resize 
+  //MENU resize
+      var wid = $(window).width();
+
       var touch = $('#touch_menu');
       var menu = $('nav');
 
@@ -8,16 +10,38 @@
           menu.slideToggle();
       });
       $(window).resize(function() {
-          var wid = $(window).width();
           if (wid > 760 && menu.is(':hidden')) {
               menu.removeAtrr('style')
           }
       })
+
       $(".toggle-mnu").click(function() {
           $(this).toggleClass("on");
           $(".main-mnu").slideToggle();
           return false;
       });
+
+
+
+//SECTION_SERVICES menu
+      var touchServices = $('.services_nav_cont>a');
+      var menuServices = $('.services_nav_cont ul');
+      $(document).click( function(event){
+      	if( $(event.target).closest(menuServices).length )
+      	return;
+      	$(menuServices).slideUp(300);
+      	event.stopPropagation();
+      });
+      $(touchServices).click( function() {
+          
+      		$(this).siblings(menuServices).slideToggle(300);
+      		return false;
+      	});
+
+        $(touchServices).click(function(){
+            $(this).toggleClass('gort');
+        });
+
 
 
   })
